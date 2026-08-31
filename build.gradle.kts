e0c71e2457f50d1d99b499dc31f39e8b8edff4f7
Ariel Faridja <arielfrja@gmail.com>
2026-08-31 06:35:12 +0300

    build: add conditional AAPT2 override for Termux support

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,11 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.google.services) apply false
 }
+
+// Support for Termux development: Use local AAPT2 if running in Termux environment
+val termuxAapt2 = file("/data/data/com.termux/files/home/android-sdk/build-tools/34.0.4/aapt2")
+if (termuxAapt2.exists()) {
+    allprojects {
+        extra.set("android.aapt2FromMavenOverride", termuxAapt2.absolutePath)
+    }
+}
